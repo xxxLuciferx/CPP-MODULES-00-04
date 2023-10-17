8d88ec79845a2604dcd4245124d03e4d33dac64f
Khalil Haimer <khaimer@c1r8p1.1337.ma>
2023-10-17 15:51:21 +0100

    Starting module02

---
+++ Module02/ex00/Fixed.hpp
@@ -0,0 +1,21 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+    private:
+        int fixedPointValue;
+        static const int fractionalBits = 8;
+    public:
+        Fixed();
+        ~Fixed();
+
+        // Fixed(const Fixed &fixed);
+        // Fixed &operator=(const Fixed &fixed);
+        // int getRawBits(void) const;
+        // void setRawBits(int const raw);
+};
+
+#endif
