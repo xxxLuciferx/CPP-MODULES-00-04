6cccdddc8f0b3b7b420adf4dee4ba6bd2b11013a
Khalil Haimer <khaimer@c1r4p3.1337.ma>
2023-11-02 20:07:40 +0100

    M03ex00 finished

--- Module03/ex00/ClapTrap.hpp
+++ Module03/ex00/ClapTrap.hpp
@@ -6,7 +6,31 @@
 /*   By: khaimer <khaimer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 18:04:39 by khaimer           #+#    #+#             */
-/*   Updated: 2023/11/02 18:04:40 by khaimer          ###   ########.fr       */
+/*   Updated: 2023/11/02 18:49:13 by khaimer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
+class ClapTrap 
+{
+    private:
+        std::string Name;
+        unsigned int Hit_points;
+        unsigned int Energy_points;
+        unsigned int Attack_damage;
+
+    public :
+        ClapTrap();
+        ClapTrap(const std::string& name);
+        ClapTrap(const ClapTrap& other);
+
+        ClapTrap& operator=(const ClapTrap& other);
+        
+        void attack(const std::string& target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+
+        ~ClapTrap();
+};
--- Module03/ex00/main.cpp
+++ Module03/ex00/main.cpp
@@ -6,7 +6,36 @@
 /*   By: khaimer <khaimer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 18:04:43 by khaimer           #+#    #+#             */
-/*   Updated: 2023/11/02 18:04:44 by khaimer          ###   ########.fr       */
+/*   Updated: 2023/11/02 18:49:50 by khaimer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+int main() {
+    // Test constructors
+    ClapTrap clap1("Clap1");
+    ClapTrap clap2("Clap2");
+
+    // Test copy constructor
+    ClapTrap clap3(clap1);
+    std::cout << "-----------" << std::endl;
+
+    // Test assignment operator
+    clap2 = clap1;
+    std::cout << "-----------" << std::endl;
+
+    // Test self-assignment (this should be gracefully handled without duplicating actions)
+    // clap1 = clap1;
+    // std::cout << "-----------" << std::endl;
+
+    // Test attack, takeDamage, and beRepaired functions
+    clap1.attack("Enemy");
+    clap1.takeDamage(5);
+    clap1.beRepaired(3);
+    std::cout << "-----------" << std::endl;
+
+    // Destructor calls will be printed when the objects go out of scope at the end of the main function
+
+    return 0;
+}
